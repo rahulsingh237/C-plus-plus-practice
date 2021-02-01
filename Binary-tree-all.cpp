cc36d72e7bc9da027084d225ac2aa54e2695d854
Rahul Veer Singh <singh.rahul220499@gmail.com>
2021-02-01 22:01:50 +0530

    update

--- Binary-tree-all.cpp
+++ Binary-tree-all.cpp
@@ -1,3 +1,6 @@
+//31 jan 2021 level up pep coding
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -104,3 +107,52 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     }
     return lca;
 }
+
+//--------------------------------------------------------------------------------------------------
+
+void printKDown(TreeNode* node, TreeNode* block, int depth, vector<int>& ans) {
+    if (node == nullptr || depth < 0 || node == block)
+        return;
+
+    if (depth == 0) {
+        ans.push_back(node->val);
+        return;
+    }
+
+    printKDown(node->left, block, depth - 1, ans);
+    printKDown(node->right, block, depth - 1, ans);
+}
+//863 leetcode
+vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+    
+    vector<TreeNode*> v;
+    roottonode(root, target, v);
+
+    vector<int> ans;
+    TreeNode* blockNode = nullptr;
+
+    for (int i = 0; i < v.size(); i++) {
+        printKDown(v[i], blockNode, K - i, ans);
+        blockNode = v[i];
+    }
+    return ans;
+}
+
+int rootToNodeDistance(TreeNode* node, TreeNode* data) {
+    if (node == nullptr)
+        return -1;
+    if (node == data)
+        return 0;
+
+    int lans = rootToNodeDistance(node->left, data);
+    if (lans != -1)
+        return lans + 1;
+
+    int rans = rootToNodeDistance(node->right, data);
+    if (rans != -1)
+        return rans + 1;
+
+    return -1;
+}
+
+//https://www.geeksforgeeks.org/burn-the-binary-tree-starting-from-the-target-node/
